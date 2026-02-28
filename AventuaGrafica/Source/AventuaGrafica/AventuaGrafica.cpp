379c48e9de8b61efb0fb06a687323d9e9bf8094e
Vyktor <victureca3@gmail.com>
2026-02-28 19:00:21 +0100

    Unreal Proyecto

---
+++ AventuaGrafica/Source/AventuaGrafica/AventuaGrafica.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AventuaGrafica.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AventuaGrafica, "AventuaGrafica" );
